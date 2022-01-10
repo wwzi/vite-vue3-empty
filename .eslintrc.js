f87b6f3004de03dc9623093f29d07d32b3f6d381
wwzi <wayenzhong@gmail.com>
2022-01-10 13:57:01 +0800

    feat: 代码规范
    EditorConfig & Prettier & ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    // https://blog.csdn.net/zhanye88/article/details/121644706
+    'vue/setup-compiler-macros': true,
+  },
+  extends: [
+    'airbnb-base',
+    // https://stackoverflow.com/questions/68776902/should-i-use-pluginvue-essential-and-pluginvue-vue3-recommended-when-linti/68776947
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'import/no-unresolved': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['./*.js', './*.ts'] }],
+    'vue/no-multiple-template-root': 0,
+  },
+};
--- README.md
+++ README.md
@@ -2,14 +2,39 @@
 https://juejin.cn/post/6951649464637636622
 
 初始化目录
-+ npm init @vitejs/app
-+ vite-vue3-empty
-+ vue -> vue-ts
-+ 修改 vite.config.ts
-+ 安装 @types/node
+
+- `npm init @vitejs/app`
+- `vite-vue3-empty`
+- `vue -> vue-ts`
+- 修改 `vite.config.ts`
+- `npm i @types/node -D`
 
 重置默认样式
-+ /src/styles/reset.css
 
-CSS预编译
-+ npm i less -D // less 好安装一点(
+- `/src/styles/reset.css`
+
+CSS 预编译
+
+- `npm i less -D` // less 好安装一点(
+
+代码规范
+
+EditorConfig
+
+- 安装 `EditorConfig` 插件
+- 增加 `.editorconfig` 文件
+
+Prettier
+
+- 安装 `prettier`插件
+- `npm i prettier -D`
+- 增加 `.prettierrc` 文件
+
+````
+
+ESLint
++ ```安装 ESLint 插件```
++ ```npm i eslint -D```
++ ```npx eslint --init```
++ 修改 ```。eslintrc.js``` 文件
+````
